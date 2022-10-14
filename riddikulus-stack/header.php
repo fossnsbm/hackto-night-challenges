8e4c6a33e1204d63d1250d637474e3a2de45a8ba
chamodhpereira <chamodhperera@outlook.com>
2022-10-15 00:39:16 +0530

    Navbar Initial Commit

---
+++ riddikulus-stack/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+    <title></title>
+</head>
+<body>
+<nav class="navbar navbar-expand-lg bg-dark">
+  <div class="container-fluid">
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mt-2 ">
+        <li class="nav-item">
+            <h6 class="h6"><a class="nav-link active text-white" aria-current="page" href="#">Home</a></h6>
+          
+        </li>
+        <li class="nav-item">
+            <h6 class="h6"><a class="nav-link text-white" href="#">Sell</a></h6>
+            
+        </li>
+        <li class="nav-item">
+            <h6 class="h6"><a class="nav-link text-white" href="#">Cart</a></h6>
+          
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-primary" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
